c24ac1b8d66f9be69423f8927da14471d7d1e256
Gemsen <64825265+Gemsen@users.noreply.github.com>
2020-08-06 15:32:59 +0700

    Update j.php

--- j.php
+++ j.php
@@ -6,7 +6,7 @@ echo color("white","          [•]  Time  : ".date('[d-m-Y] [H:i:s]')."   \n");
 echo color("green","                          @Chuex80 \n");
 echo color("white","* Token: ");
 $token = trim(fgets(STDIN));
-echo color("white","? Mau Klaim Voucher?(Y/N) ");
+echo color("red","!] Klaim Voucher?(Y/N) ");
         $pilihan = trim(fgets(STDIN));
         if($pilihan == "y" || $pilihan == "Y"){
         echo "\n".color("nevy","!] Tikor DENPASAR ");
@@ -87,14 +87,14 @@ echo color("white","? Mau Klaim Voucher?(Y/N) ");
         $expired11 = getStr1('"expiry_date":"','"',$cekvoucher,'11');
         $expired12 = getStr1('"expiry_date":"','"',$cekvoucher,'12');
         setpin:
-         echo "\n".color("white","? Mau set pin?: y/n ");
+         echo "\n".color("pink","+] Mau set pin?: y/n ");
          $pilih1 = trim(fgets(STDIN));
          if($pilih1 == "y" || $pilih1 == "Y"){
          //if($pilih1 == "y" && strpos($no, "628")){
          echo color("yellow","PIN ANDA = 112233")."\n";
          $data2 = '{"pin":"112233"}';
          $getotpsetpin = request("/wallet/pin", $token, $data2, null, null, $uuid);
-         echo "Otp set pin: ";
+         echo "Otp set pin : ";
          $otpsetpin = trim(fgets(STDIN));
          $verifotpsetpin = request("/wallet/pin", $token, $data2, null, $otpsetpin, $uuid);
          echo $verifotpsetpin;
