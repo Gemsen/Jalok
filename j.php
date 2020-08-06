08384006522195fa140879dd0465f6945d65ba55
Gemsen <64825265+Gemsen@users.noreply.github.com>
2020-08-07 02:10:46 +0700

    Update j.php

--- j.php
+++ j.php
@@ -27,7 +27,7 @@ echo "\n".color("white","?] Mau Redeem Voucher?: y/n ");
         echo color("nevy",".");
         sleep(10);
         }
-        $code1 = request2('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD0508"}');
+        $code1 = request2('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD0508"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","+] Message: ".$message);
